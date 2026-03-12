84bbd13288ecbf8473fa9e4852fa7b41d749d9ff
unknown <a20222923@pucp.edu.pe>
2026-03-12 18:04:15 -0500

    comenzando lab 4 xd

---
+++ src/main/java/com/example/solulab4/SoluLab4Application.java
@@ -0,0 +1,13 @@
+package com.example.solulab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoluLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoluLab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solulab4/SoluLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solulab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoluLab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
